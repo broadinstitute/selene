23dce7d48e4d286dd8b85f274882e6057c98f725
curoli <curoli@gmail.com>
2021-09-15 16:06:14 -0400

    Merging calculated and cached annotations, complete. Advancing version to 2.2.0

--- src/mion/eval/predef.rs
+++ src/mion/eval/predef.rs
@@ -6,6 +6,7 @@ use crate::mion::eval::builtin::merge_all_files::MergeAllFiles;
 use crate::mion::eval::builtin::vep::Vep;
 use crate::mion::eval::builtin::replace_file_name::ReplaceFileName;
 use crate::mion::eval::builtin::pick_vep_results::PickVepResults;
+use crate::mion::eval::builtin::merge_sorted_files::MergeSortedFiles;
 
 pub(crate) fn predef_symbols() -> Symbols {
     Symbols::new()
@@ -16,4 +17,5 @@ pub(crate) fn predef_symbols() -> Symbols {
         .with_function_entry(Box::new(Vep {}))
         .with_function_entry(Box::new(ReplaceFileName {}))
         .with_function_entry(Box::new(PickVepResults {}))
+        .with_function_entry(Box::new(MergeSortedFiles {}))
 }
